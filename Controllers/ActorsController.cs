cb229b5308c1a41111794c92be3d41d19c218bee
Di Depan Layar <albinmaulana@yahoo.com>
2023-04-14 14:21:34 +0700

    Feature: Add Actors Controller

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_eTickets.Data;
+using System.Linq;
+
+namespace Movie_eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
