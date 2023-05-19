c24994aa2e67e8c8c8a1ac228fd408bd98844c6e
Di Depan Layar <albinmaulana@yahoo.com>
2023-05-19 16:40:03 +0700

    Feature: Add Producers View

--- Controllers/ProducersController.cs
+++ Controllers/ProducersController.cs
@@ -15,7 +15,7 @@ namespace Movie_eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var allProducers = await _context.Producers.ToListAsync();
-            return View();
+            return View(allProducers);
         }
     }
 }
