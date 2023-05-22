e3999634b213fac454b186dda99e4c03539a3692
Di Depan Layar <albinmaulana@yahoo.com>
2023-05-22 12:29:08 +0700

    Feature: Add IActorsService Interface

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,19 @@
+﻿using Movie_eTickets.Models;
+using System.Collections.Generic;
+
+namespace Movie_eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        // Get All Actor
+        IEnumerable<Actor> GetAll();
+        // Get Actor by ID
+        Actor GetById(int id);
+        // Add Actor to Database
+        void add(Actor actor);
+        // Update Actor to Database
+        Actor Update(int id, Actor newActor);
+        // Delete Actor from Database
+        void Delete(int id);
+    }
+}
