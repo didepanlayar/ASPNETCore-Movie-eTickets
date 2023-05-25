ff73cb9b88593920bc5fc9045acabf47364944e2
Di Depan Layar <albinmaulana@yahoo.com>
2023-05-25 11:32:19 +0700

    Feature: Add EntityBaseRepository

---
+++ Data/Base/IEntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Movie_eTickets.Data.Base
+{
+    public interface IEntityBase
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movie_eTickets.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T: class, IEntityBase, new()
+    {
+        // Get All Data
+        Task<IEnumerable<T>> GetAllAsync();
+        // Get Data by ID
+        Task<T> GetByIdAsync(int id);
+        // Add Data to Database
+        Task AddAsync(T entity);
+        // Update Data to Database
+        Task<T> UpdateAsync(int id, T entity);
+        // Delete Data from Database
+        Task DeleteAsync(int id);
+    }
+}
