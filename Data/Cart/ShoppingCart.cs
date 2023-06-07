6400aae3eb24dd5655240124046b58af21d1eb08
Di Depan Layar <albinmaulana@yahoo.com>
2023-06-07 12:26:32 +0700

    Feature: Add Shopping Cart Class

---
+++ Data/Cart/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie_eTickets.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Movie_eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+    }
+}
