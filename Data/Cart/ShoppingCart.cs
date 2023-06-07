ba421d413ee1a695830901c55ad5ef704f654411
Di Depan Layar <albinmaulana@yahoo.com>
2023-06-07 12:51:22 +0700

    Feature: Add Item to Shopping Cart Class

--- Data/Cart/ShoppingCart.cs
+++ Data/Cart/ShoppingCart.cs
@@ -14,6 +14,23 @@ namespace Movie_eTickets.Data.Cart
         {
             _context = context;
         }
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+            if(shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    Movie = movie,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            } else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
