165504abaaede8f3dcfd560834a32791c143771d
Di Depan Layar <albinmaulana@yahoo.com>
2023-06-21 10:55:15 +0700

    Feature: Add Orders Service

---
+++ Data/Services/IOrdersService.cs
@@ -0,0 +1,12 @@
+﻿using Movie_eTickets.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movie_eTickets.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace Movie_eTickets
             services.AddScoped<IProducersService, ProducersService>();
             services.AddScoped<ICinemasService, CinemasService>();
             services.AddScoped<IMoviesService, MoviesService>();
+            services.AddScoped<IOrdersService, OrdersService>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
             services.AddSession();
