818e3f1cab701751b31588f93cbb5f17872291ad
Di Depan Layar <albinmaulana@yahoo.com>
2023-07-24 11:25:56 +0700

    Feature: Add Account Controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movie_eTickets.Data;
+using Movie_eTickets.Models;
+
+namespace Movie_eTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
